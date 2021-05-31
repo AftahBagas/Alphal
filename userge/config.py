01f883b056f27b273d66a3992d762bbdf4967fe9
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 22:48:59 +0700

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -1,11 +1,4 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
+# alfareza
 # All rights reserved.
 
 __all__ = ['Config', 'get_version']
@@ -97,7 +90,7 @@ def get_version() -> str:
             return f"{ver}-Grade.{len(diff)}"
     else:
         diff = list(_REPO.iter_commits(
-            f'{Config.UPSTREAM_REMOTE}/master..HEAD'))
+            f'{Config.UPSTREAM_REMOTE}/alpha..HEAD'))
         if diff:
             return f"{ver}-GAY.{len(diff)}"
     return ver
