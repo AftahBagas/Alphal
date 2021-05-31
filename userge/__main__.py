b600dd382c02f0f355f6bc5d8793a9de32f1c7dd
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 22:46:09 +0700

    Update __main__.py

--- userge/__main__.py
+++ userge/__main__.py
@@ -1,14 +1,7 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
+# alfareza
 # All rights reserved.
 
-from userge import userge
+from alpha import alpha
 
 if __name__ == "__main__":
-    userge.begin()
+    alpha.begin()
