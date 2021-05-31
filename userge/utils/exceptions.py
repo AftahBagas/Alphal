824a132c19d3af63e62f47b3b81ad2b410703a43
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 22:54:20 +0700

    Update exceptions.py

--- userge/utils/exceptions.py
+++ userge/utils/exceptions.py
@@ -1,11 +1,4 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
+# alpha
 # All rights reserved.
 
 
@@ -18,4 +11,4 @@ class ProcessCanceled(Exception):
 
 
 class UsergeBotNotFound(Exception):
-    """ raise if userge bot not found """
+    """ raise if alphaz plugins bot not found """
